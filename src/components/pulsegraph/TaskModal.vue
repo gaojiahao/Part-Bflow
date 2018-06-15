c1519afa68cbb256b552eda3561c805466b4cecc
谭国正 <zheng.tan@refordom.com>
2018-06-15 18:11:05 +0800

    修改bug

--- src/components/pulsegraph/TaskModal.vue
+++ src/components/pulsegraph/TaskModal.vue
@@ -98,7 +98,7 @@ export default {
       pageTotal: 0, //table总数
       pageSize: 6,
       currentPage: 1, //table当前页
-      type: "mytask",
+      type: "myToDo",
       modelState: false,
       transCode: "",
       creatorName: "",

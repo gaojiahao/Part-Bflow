fe4468155ef85c5610f37388c5ac14f29dc86a40
snack.huang <snack.huang@refordom.com>
2018-07-24 17:28:14 +0800

    change css

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -6,7 +6,7 @@ axios.defaults.timeout = 10000
 
 axios.interceptors.request.use(config => {
   let token = getToken();
-  config.headers.Authorization = 'ddfe1332935048e9b0d0dd73e922fbdd';
+  config.headers.Authorization = token;
   return config;
 }, err => {
   return Promise.reject(err)

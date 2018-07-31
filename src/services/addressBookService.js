c6707a8b8e8cff6258b88bb38dfdf35e9b4c63d3
谭国正 <zheng.tan@refordom.com>
2018-07-31 16:11:55 +0800

    添加api接口

--- src/services/addressBookService.js
+++ src/services/addressBookService.js
@@ -1,5 +1,7 @@
 import {request} from './fetch';
 
+/************  用户  **************/
+
 /**
  * @author XiaoYing
  * @description 获取上级用户数据
@@ -9,3 +11,46 @@ export const getHighUserData = (userId,pageSize,currentPage) => request('/H_role
     page: currentPage,
     limit: pageSize
 });
+
+
+/************  组织  **************/
+
+/**
+ * @author GuoZheng
+ * @description 获取组织成员信息
+ */
+export const getUsersByGroupId = (groupId,currentPage,pageSize) => request('/H_roleplay-si/ds/getUsersByGroupId',{
+    groupId: groupId,
+    page: currentPage,
+    limit: pageSize,
+    start:0,
+});
+
+/**
+ * @author GuoZheng
+ * @description 获取组织权限
+ */
+export const getObjectPermission2Oneself = (objectName,objectId,currentPage,pageSize) => request('/H_roleplay-si/ds/getObjectPermission2Oneself',{
+    objectName:objectName,
+    objectId: objectId,
+    page: currentPage,
+    limit: pageSize,
+    start:0,
+});
+
+/**
+ * @author GuoZheng
+ * @description 获取用户列表
+ */
+export const listUsers = (currentPage,pageSize) => request('/H_roleplay-si/ds/listUsers',{
+    entityId:20000,
+    page: currentPage,
+    limit: pageSize,
+    start:0,
+});
+
+
+/************  职位  **************/
+
+
+/************  职位  **************/

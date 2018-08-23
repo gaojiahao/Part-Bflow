cfd6c3cc3cc18c276e0e695db09bec52237a03f2
wangxiaoying <cherry.wang@refordom.com>
2018-08-23 14:03:38 +0800

    修改公司关闭跳转路径

--- src/views/addressBook/companyInfo/baseInfo/baseInfo.vue
+++ src/views/addressBook/companyInfo/baseInfo/baseInfo.vue
@@ -346,7 +346,7 @@ export default {
         title: "系统提示",
         content: "确认要关闭当前页面吗？",
         onOk: () => {
-          this.$router.push({ path: "/addressBook/companyInfo/board" });
+          location.href = '/Site/index.html#page/companys';
         }
       });
     }

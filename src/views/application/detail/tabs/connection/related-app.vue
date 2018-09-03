53f740780f692fbd9606d56689ccc5761cb80388
wangxiaoying <cherry.wang@refordom.com>
2018-09-03 10:54:42 +0800

    修改应用详情科目关系数据归集判断

--- src/views/application/detail/tabs/connection/related-app.vue
+++ src/views/application/detail/tabs/connection/related-app.vue
@@ -6,17 +6,22 @@
 <div class="bg_ff martop20">
 
     <Row class="app-resource-group-title">
-      <h3>相关应用</h3>
+      <h3>相关应用
+        <a @click="saveAppData">保存</a>
+      </h3>
     </Row>
     <Row class="related-app-content" :gutter="8">
       <!-- <Table :columns="columns" :data="relatedApps" size="small"></Table> -->
       <draggable v-model="relatedApps" :options="dragOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false">
         <transition-group type="transition" :name="'flip-list'">
-        <Col :xs="24" :sm="12" :md="8" :lg="6" v-for="(app,index) of relatedApps" :key="index">
+        <Col :xs="24" :sm="12" :md="6" :lg="4" v-for="(app,index) of relatedApps" :key="index" style="margin-bottom:5px">
           <Card class="app-card">
             <img class="card-img" :src="app.icon" />
-            <b class="card-name">{{ app.relationAppName }}</b>
-            <span class="card-type">{{ app.relationAppType }}</span>
+            <span class="card-right">
+              <b class="card-name">{{ app.relationAppName }}</b>
+              <span class="card-type">{{ app.relationAppType }}</span>
+              <Checkbox @on-change="changeAppStatus(app,index)" class="card-check" v-model="single"></Checkbox>
+            </span>
           </Card>
         </Col>
         </transition-group>
@@ -45,6 +50,7 @@ export default {
       editable: true,
       isDragging: false,
       delayedDragging: false,
+      single: true,
       columns: [
         {
           title: "应用名称",
@@ -102,6 +108,14 @@ export default {
       return (
         (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
       );
+    },
+    //相关应用排序保存
+    saveAppData() {
+      console.log(this.relatedApps);
+    },
+    //启用禁用相关应用
+    changeAppStatus(app,index) {
+      console.log(app,index);
     }
   },
   created() {

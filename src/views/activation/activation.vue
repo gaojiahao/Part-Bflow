9bb2819caf6b58ecc7ec93b797ff1b8e5ac40a5d
wangxiaoying <cherry.wang@refordom.com>
2018-09-26 19:19:14 +0800

    修改用户激活页面布局

--- src/views/activation/activation.vue
+++ src/views/activation/activation.vue
@@ -4,33 +4,31 @@
 
 <template>
     <div class="active-body">
-        <div class="active" @keydown.enter="handleSubmit">
-            <div class="active-con">
-                <div style="text-align:center;">
-                    <img src="static/favicon.ico" style="width: 40px;"></img>
-                    <b>Roletask</b>
-                </div>
-                <p class="active-welcome">欢迎激活账号</p>
-                <div class="form-con">
-                    <Form ref="activeForm" :model="form" :rules="rules">
-                        <FormItem prop="userCode" label="账号：">
-                            <Input v-model="form.userCode" placeholder="请输入账号"></Input>
-                        </FormItem>
-                        <FormItem prop="userName" label="昵称：">
-                            <Input v-model="form.userName" placeholder="请输入昵称"></Input>
-                        </FormItem>
-                        <FormItem prop="password" label="密码：">
-                            <Input type="password" v-model="form.password" placeholder="请输入密码"></Input>
-                        </FormItem>
-                        <FormItem prop="confirmPassword" label="确认密码：">
-                            <Input type="password" v-model="form.confirmPassword" placeholder="请确认密码"></Input>
-                        </FormItem>
-                        <FormItem>
-                            <Button @click="handleSubmit" type="primary" long>激活</Button>
-                        </FormItem>
-                    </Form>
-                    <p class="active-tip">请输入账号和密码</p>
-                </div>
+        <div class="active-con">
+            <div style="text-align:center;">
+                <img src="static/favicon.ico" style="width: 40px;"></img>
+                <b>Roletask</b>
+            </div>
+            <p class="active-welcome">欢迎激活账号</p>
+            <div class="form-con">
+                <Form ref="activeForm" :model="form" :rules="rules">
+                    <FormItem prop="userCode" label="账号：">
+                        <Input v-model="form.userCode" placeholder="请输入账号"></Input>
+                    </FormItem>
+                    <FormItem prop="userName" label="昵称：">
+                        <Input v-model="form.userName" placeholder="请输入昵称"></Input>
+                    </FormItem>
+                    <FormItem prop="password" label="密码：">
+                        <Input type="password" v-model="form.password" placeholder="请输入密码"></Input>
+                    </FormItem>
+                    <FormItem prop="confirmPassword" label="确认密码：">
+                        <Input type="password" v-model="form.confirmPassword" placeholder="请确认密码"></Input>
+                    </FormItem>
+                    <FormItem>
+                        <Button @click="handleSubmit" type="primary" long>激活</Button>
+                    </FormItem>
+                </Form>
+                <p class="active-tip">请输入账号和密码</p>
             </div>
         </div>
     </div>

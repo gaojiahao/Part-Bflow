ab60a61f37459f96f61e9efcc0058bbb280f6611
zhaohuai <huai.zhao@refordom.com>
2018-10-10 15:02:42 +0800

    流程状态管理功能

--- src/views/application/detail/detail.vue
+++ src/views/application/detail/detail.vue
@@ -1,21 +1,12 @@
 <style lang="less" scoped>
-  @import './detail.less';
+@import "./detail.less";
 </style>
 
 <template>
   <div class="app-details">
 
-
     <!-- 应用详情信息 -->
-    <app-info 
-      :listId="this.$route.params.listId" 
-      :enforData="enforData" 
-      :isAdmin="isAdmin" 
-      :isCompanyAdmin="isCompanyAdmin" 
-      :appData="appData" 
-      @reloadData="reloadData" 
-      @changeAdmin="changeAdmin" 
-      @enabledForbiddenAppPermission="enabledForbiddenAppPermission">
+    <app-info :listId="this.$route.params.listId" :enforData="enforData" :isAdmin="isAdmin" :isCompanyAdmin="isCompanyAdmin" :appData="appData" @reloadData="reloadData" @changeAdmin="changeAdmin" @enabledForbiddenAppPermission="enabledForbiddenAppPermission">
     </app-info>
 
     <div class="rfd-tab">
@@ -25,45 +16,30 @@
           <change-log :listId="listId" :isAdmin="isAdmin"></change-log>
         </TabPane>
         <TabPane label="数据分析" name="name2">
-          <log-instance 
-            :isAdmin="isAdmin" 
-            :isAddress="isAddress"
-            :appType="appType">
+          <log-instance :isAdmin="isAdmin" :isAddress="isAddress" :appType="appType">
           </log-instance>
         </TabPane>
         <TabPane label="互动" name="name3">
           <!-- 管理员自评 -->
           <admintrstor-assessment :isAdmin="isAdmin"></admintrstor-assessment>
           <!-- 用户评论 -->
-          <user-comments ></user-comments>
+          <user-comments></user-comments>
         </TabPane>
-        <TabPane 
-        :disabled="!(isAdmin || isCompanyAdmin)" 
-        :label="(isAdmin || isCompanyAdmin)?'资源':''" 
-        name="name4">
-          <permission-source 
-            :appType="appType" 
-            :isAdmin="isAdmin" 
-            :isAddress="isAddress" 
-            :enabledForbidden="enabledForbidden">
+        <TabPane :disabled="!(isAdmin || isCompanyAdmin)" :label="(isAdmin || isCompanyAdmin)?'资源':''" name="name4">
+          <permission-source :appType="appType" :isAdmin="isAdmin" :isAddress="isAddress" :enabledForbidden="enabledForbidden">
           </permission-source>
         </TabPane>
-        <TabPane 
-        :disabled="!(isAdmin || isCompanyAdmin) && appType === 'subject'" 
-        :label="((isAdmin || isCompanyAdmin) && appType !== 'subject')?'连接':''" 
-        name="name5">
+        <TabPane :disabled="!(isAdmin || isCompanyAdmin) && appType === 'subject'" :label="((isAdmin || isCompanyAdmin) && appType !== 'subject')?'连接':''" name="name5">
           <!-- 应用科目 -->
           <div class="app-sub">
-            <app-subject 
-              v-if="!isAddress" 
-              :isAdmin="isAdmin">
+            <app-subject v-if="!isAddress" :isAdmin="isAdmin">
             </app-subject>
           </div>
           <!-- 相关应用 -->
-          <related-app 
-            v-if="!isAddress" 
-            :isAdmin="isAdmin">
+          <related-app v-if="!isAddress" :isAdmin="isAdmin">
           </related-app>
+          <!-- 流程状态管理-->
+          <process v-if="!isAddress" :isAdmin="isAdmin"></process>
           <!-- API -->
           <app-api></app-api>
         </TabPane>
@@ -79,11 +55,13 @@ import LogInstance from "./tabs/analysis/log-instance";
 import AppInfo from "./information/information";
 import AppSubject from "./tabs/connection/subject";
 import AppApi from "./tabs/connection/api";
-import RelatedApp from  './tabs/connection/related-app';
+import RelatedApp from "./tabs/connection/related-app";
 import AdmintrstorAssessment from "./tabs/exchange/admintrstor-assessment";
 import UserComments from "./tabs/exchange/user-comments";
 import PermissionSource from "./tabs/permission/permission";
 import ChangeLog from "./tabs/changelog/change-log";
+import Process from "./tabs/permission/process/process";
+
 export default {
   name: "detail",
   components: {
@@ -95,7 +73,8 @@ export default {
     AppInfo,
     LogInstance,
     RelatedApp,
-    ChangeLog
+    ChangeLog,
+    Process
   },
   data() {
     return {
@@ -106,7 +85,7 @@ export default {
       isAdmin: false,
       isCompanyAdmin: false,
       isAddress: false,
-      appType: '',
+      appType: "",
       enabledForbidden: -1
     };
   },
@@ -121,42 +100,45 @@ export default {
     //获取应用详情信息
     getAppInfoDatas() {
       let uniqueId = this.listId,
-          currentUser = this.$currentUser,
-          currentUserIds = [];
+        currentUser = this.$currentUser,
+        currentUserIds = [];
       //请求应用详情信息
       getListData(uniqueId).then(res => {
         this.appData = res[0];
         this.appType = this.appData.type;
-        
+
         //获取当前登录用户角色id
         currentUser.isSysRoleList.forEach(val => {
           currentUserIds.push(val.id);
         });
         //判断当前用户是否有当前应用权限
-        if(this.appData.administrator){
-          if(currentUser.nickname === this.appData.administrator && currentUserIds.indexOf(1) > -1){
+        if (this.appData.administrator) {
+          if (
+            currentUser.nickname === this.appData.administrator &&
+            currentUserIds.indexOf(1) > -1
+          ) {
             this.isAdmin = true;
             this.isCompanyAdmin = true;
-          }else if(currentUser.nickname == this.appData.administrator){
+          } else if (currentUser.nickname == this.appData.administrator) {
             this.isAdmin = true;
-          }else if(currentUserIds.indexOf(1) > -1){
+          } else if (currentUserIds.indexOf(1) > -1) {
             this.isCompanyAdmin = true;
             this.isAdmin = false;
-          }else{
+          } else {
             this.isAdmin = false;
             this.isCompanyAdmin = false;
           }
-        }else{
-          if(currentUserIds.indexOf(1) > -1){
+        } else {
+          if (currentUserIds.indexOf(1) > -1) {
             this.isAdmin = true;
             this.isCompanyAdmin = true;
-          }else{
+          } else {
             this.isAdmin = false;
             this.isCompanyAdmin = false;
           }
         }
         //判断是否是通讯录并控制其相关应用权限
-        if(this.appData.type === 'hr'){
+        if (this.appData.type === "hr") {
           this.isAddress = true;
         }
       });
---
+++ src/views/application/detail/tabs/permission/process/process.vue
@@ -0,0 +1,255 @@
+<style lang="less" scoped>
+@import "./process.less";
+</style>
+<template>
+  <div class="app-process">
+    <Row class="app-process-title">
+      <h3>流程状态管理</h3>
+      <span class="warning-color marlr">管理员用户可以管理状态
+        <a @click="addProcess">添加</a>
+      </span>
+    </Row>
+    <Row class="app-process-table">
+      <Modal v-model="showModal" title="新增流程状态" @on-ok="addProcessStatus" @on-cancel="cancel">
+        <Form :model="processInfoItem" ref="processInfoItem" :label-width="100" :rules="ruleValidate">
+          <FormItem label="流程状态值:" prop="fieldValue">
+            <Input v-model="processInfoItem.fieldValue"></Input>
+          </FormItem>
+          <FormItem label="流程状态编码:" prop="fieldCode">
+            <Input v-model="processInfoItem.fieldCode"></Input>
+          </FormItem>
+          <FormItem label="排序:" prop="sort">
+            <Input v-model="processInfoItem.sort"></Input>
+          </FormItem>
+        </Form>
+      </Modal>
+      <Table :columns="columns" :data="processData" size="small"></Table>
+      <div style="margin: 10px;overflow: hidden">
+        <div class="fr">
+          <Page @on-page-size-change="onPageSizeChange" :total="dataTotal" show-elevator show-sizer :current="pageIndex" :page-size="pageSize" @on-change="pageChange" size="small" show-total></Page>
+        </div>
+      </div>
+    </Row>
+  </div>
+</template>
+<script>
+import {
+  getProcessStatus,
+  deleteProcessStatus,
+  updateProcessStatus,
+  addProcessStatus
+} from "@/services/appService.js";
+export default {
+  data() {
+    return {
+      columns: [
+        {
+          title: "流程状态",
+          key: "fieldValue",
+          width: 180,
+          render: (h, params) => {
+            if (params.row.$isEdit) {
+              return h("input", {
+                domProps: {
+                  value: params.row.fieldValue
+                },
+                on: {
+                  input: function(event) {
+                    params.row.fieldValue = event.target.value;
+                  }
+                }
+              });
+            } else {
+              return h("div", params.row.fieldValue);
+            }
+          }
+        },
+        {
+          title: "排序",
+          key: "sort",
+          width: 180,
+          render: (h, params) => {
+            if (params.row.$isEdit) {
+              return h("input", {
+                domProps: {
+                  value: params.row.sort
+                },
+                on: {
+                  input: function(event) {
+                    params.row.sort = event.target.value;
+                  }
+                }
+              });
+            } else {
+              return h("div", params.row.sort);
+            }
+          }
+        },
+        {
+          title: "操作",
+          key: "opt",
+          width: 100,
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "error",
+                    size: "small"
+                  },
+                  on: {
+                    click: () => {
+                      this.delProcessStatus(params);
+                    }
+                  }
+                },
+                "删除"
+              )
+            ]);
+          }
+        },
+        {
+          title: "编辑",
+          key: "action",
+          width: 150,
+          align: "center",
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "success",
+                    size: "small"
+                  },
+                  on: {
+                    click: () => {
+                      if (params.row.$isEdit) {
+                        this.handleSave(params.row);
+                      } else {
+                        this.handleEdit(params.row);
+                      }
+                    }
+                  }
+                },
+                params.row.$isEdit ? "保存" : "编辑"
+              )
+            ]);
+          }
+        }
+      ],
+      processData: [],
+      dataTotal: 0,
+      pageIndex: 1,
+      pageSize: 5,
+      processValue: "",
+      showModal: false,
+      processInfoItem: {
+        fieldValue: "",
+        fieldClde: "",
+        sort: ""
+      },
+      ruleValidate: {
+        fieldValue: [
+          {
+            required: true,
+            message: "流程状态值不能为空",
+            trigger: "blur"
+          }
+        ],
+        fieldCode: [
+          {
+            required: true,
+            message: "流程状态编码不能为空",
+            trigger: "blur"
+          }
+        ],
+        sort: [
+          {
+            required: true,
+            message: "排序值不能为空",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    getProcessStatusByListId() {
+      let param = {
+        listId: this.$route.params.listId,
+        limit: this.pageSize,
+        page: this.pageIndex
+      };
+      getProcessStatus(param).then(res => {
+        this.processData = res.tableContent;
+        this.dataTotal = res.dataCount;
+      });
+    },
+    onPageSizeChange(pageSize) {
+      this.pageSize = pageSize;
+      this.getProcessStatusByListId();
+    },
+    pageChange(pageIndex) {
+      this.pageIndex = pageIndex;
+      this.getProcessStatusByListId();
+    },
+    delProcessStatus(params) {
+      let param = [params.row];
+      this.$Modal.confirm({
+        title: "确认",
+        content: "确认删除此状态？",
+        onOk: () => {
+          deleteProcessStatus(param).then(res => {
+            if (res.success === true) {
+              this.$Message.info("删除成功");
+              this.getProcessStatusByListId();
+            } else {
+              this.$Message.error(res.message);
+            }
+          });
+        }
+      });
+    },
+    handleSave(param) {
+      updateProcessStatus([param]).then(res => {
+        if (res.success === true) {
+          this.$Message.info("更新成功");
+          this.getProcessStatusByListId();
+        } else {
+          this.$Message.error(res.message);
+        }
+      });
+      this.$set(param, "$isEdit", false);
+    },
+    handleEdit(param) {
+      this.$set(param, "$isEdit", true);
+    },
+    addProcessStatus() {
+      this.processInfoItem.listId = this.$route.params.listId;
+      this.$refs["processInfoItem"].validate(valid => {
+        if (valid) {
+          addProcessStatus([this.processInfoItem]).then(res => {
+            if (res.success === true) {
+              this.$Message.info("添加成功");
+              this.getProcessStatusByListId();
+              this.showModal = false;
+            }
+          });
+        } else {
+          this.$Message.error("请输入必填项");
+        }
+      });
+    },
+    addProcess() {
+      this.showModal = true;
+    }
+  },
+  mounted() {
+    this.getProcessStatusByListId();
+  }
+};
+</script>
+
+

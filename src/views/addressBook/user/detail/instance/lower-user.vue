797e6713354abd046f8a08a511827b45eafcf850
wangxiaoying <cherry.wang@refordom.com>
2018-11-06 16:01:09 +0800

    优化代码

--- src/views/addressBook/user/detail/instance/lower-user.vue
+++ src/views/addressBook/user/detail/instance/lower-user.vue
@@ -19,23 +19,6 @@
     margin: 10px;
     overflow: hidden;
   }
-  .app-search {
-    margin-bottom: 5px;
-    .app-search-icon {
-      font-size: 1rem;
-      color: #39f;
-      display: inline-block;
-      cursor: pointer;
-    }
-  }
-  .page-selection-warp {
-    width: 100%;
-    height: 100%;
-    min-height: 30px;
-    background-color: #e6e6e6;
-    margin-bottom: -8px;
-    padding: 1px 5px;
-  }
 </style>
 
 <template>
@@ -70,62 +53,12 @@
             </div>
           </div>
         </div>
-        <Modal
-            v-model="showModal"
-            title="选择用户"
-            @on-ok="addLowUser"
-            :styles="{top: '15px'}"
-            width="1000">
-            <div class="app-search">
-              <Input 
-                @on-search="userFilter" 
-                :search="true" 
-                v-model="searchValue" 
-                placeholder="搜索工号或姓名" 
-                style="width: 300px">
-              </Input>
-              <p @click="userFilter" class="app-search-icon">
-                  <Button type="primary" size="small">查询</Button>
-              </p>
-            </div>
-            <Table 
-              ref="selection" 
-              @on-selection-change="onSelectionChange" 
-              @on-select-all="onSelectAll" 
-              @on-select-cancel="onSelectCancel" 
-              height="400" 
-              :loading="userLoading" 
-              :columns="userColumns" 
-              :data="userData">
-            </Table>
-            <div class="user-page">
-                <div style="float: right;">
-                  <Page 
-                    @on-page-size-change="onAllUserPageSizeChange" 
-                    :total="lowUser.usertotal" 
-                    show-elevator show-sizer 
-                    :current="lowUser.usercurrentPage" 
-                    :page-size="lowUser.allUserpageSize" 
-                    @on-change="onUserPageChange" 
-                    size="small" 
-                    show-total>
-                  </Page>
-                </div>
-            </div>
-            <div class="page-selection-warp" v-show="onPageSelection[0] ">
-              <Tag 
-                v-for="(item,index) in onPageSelection" 
-                :key="item.userId" 
-                @on-close="deleteSelectUser(item,index)" 
-                :userId="item.userId" 
-                closable 
-                type="border" 
-                color="primary" 
-                size="small">
-                {{item.nickname}}
-              </Tag>
-            </div>
-        </Modal>
+        <!-- 用户选择器 -->
+        <user-selector 
+          :showUserSelector="showModal"
+          @changeUserModalStatus="changeUserModalStatus"
+          @addUserChange="addUserChange">
+        </user-selector>
     </div>
 </template>
 
@@ -136,10 +69,13 @@ import {
   updateHighUser,
   deleteUser 
   } from "@/services/addressBookService.js";
+import UserSelector from './user-selector';
 
 export default {
   name: "lowerUser",
-  components: {},
+  components: {
+    UserSelector
+  },
   props: {
     isUpdate: {
           type: Boolean
@@ -150,15 +86,10 @@ export default {
       userId: this.$route.params.userId,
       lowUser: {
         total: 0,
-        usertotal: 0,
         currentPage: 1,
-        usercurrentPage: 1,
-        pageSize: 10,
-        allUserpageSize: 10,
+        pageSize: 10
       },
-      searchValue: '',
       loading: true,
-      userLoading: true,
       showModal: false,
       onPageSelection: [],
       columns: [
@@ -237,60 +168,15 @@ export default {
           }
         }
       ],
-      userColumns: [
-        {
-          type: "selection",
-          width: 60,
-          align: "center"
-        },
-        {
-          title: "头像",
-          key: "photo",
-          width: 80,
-          render: (h,params) => {
-            return h('div',[
-              h('Avatar',{
-                props: {
-                  src: params.row.photo?params.row.photo:'resources/images/icon/defaultUserPhoto.jpg'
-                }
-              })
-            ])
-          }
-        },
-        {
-          title: "工号",
-          key: "userCode"
-        },
-        {
-          title: "姓名",
-          key: "nickname"
-        },
-        {
-          title: "性别",
-          key: "gender",
-          render: (h, params) => {
-            let gender = params.row.gender === 1 ? "男" : "女";
-            return h("span", {}, gender);
-          }
-        },
-        {
-          title: "手机号",
-          key: "mobile",
-          width: 130
-        },
-        {
-          title: "邮箱",
-          key: "email",
-          width: 180
-        }
-      ],
       lowerUserData: [],
-      userData: [],
       selectUserData: [],
       selectLowUserData: []
     };
   },
   methods: {
+    changeUserModalStatus() {
+      this.showModal = false;
+    },
     //获取下级用户数据
     getLowerUserData() {
       if(this.userId){
@@ -314,53 +200,14 @@ export default {
       this.lowUser.pageSize = size;
       this.getLowerUserData();
     },
-    //点击切换所有用户每页显示条数
-    onAllUserPageSizeChange(size) {
-      let filter = JSON.stringify([{operator_1:"like",value_1:this.searchValue,property_1:"nickname",link:"or",operator_2:"like",value_2:this.searchValue,property_2:"userCode"},
-      {operator:"ne",value:this.userId,property:"userId"}
-      ]);
-      this.lowUser.allUserpageSize = size;
-      this.getAllUsersData(filter);
-    },
-    onUserPageChange(currentPage) {
-      let filter = JSON.stringify([{operator_1:"like",value_1:this.searchValue,property_1:"nickname",link:"or",operator_2:"like",value_2:this.searchValue,property_2:"userCode"},
-      {operator:"ne",value:this.userId,property:"userId"}
-      ]);
-      this.lowUser.usercurrentPage = currentPage;
-      this.getAllUsersData(filter);
-    },
     //选择要删除的下级用户
     selectLowUser(selection) {
       this.selectLowUserData = selection;
     },
-    //删除选择的用户
-    deleteSelectUser(item,index) {
-      this.onPageSelection.splice(index,1);
-      this.$refs.selection.data.forEach((data,i) => {
-        if(item.userId === data.userId){
-          this.$refs.selection.toggleSelect(i);
-        }
-      })
-    },
     //添加下级用户
-    addLowUser() {
-      let parentId = [];
-      if(this.onPageSelection.length > 0){
-        this.onPageSelection.forEach(val => {
-          parentId.push(val.userId);
-        });
-      }
-      if(parentId.length>0 && this.userId){
-        updateHighUser(parentId.join(','),this.userId).then(res => {
-          if(res.success){
-            this.$Message.success('更新成功');
-            this.getLowerUserData();
-            this.$emit('changeInstance');
-          }
-        }).catch(error => {
-            this.$Message.error(error.data.message);
-        })
-      }
+    addUserChange() {
+      this.getLowerUserData();
+      this.$emit('changeInstance');
     },
     //删除下级用户
     deleteLowUser() {
@@ -387,80 +234,9 @@ export default {
         });
       }
     },
-    //选择下级用户
-    onSelectionChange(selection) {
-      // this.selectUserData = selection;
-      //取消全选
-      if (selection.length === 0) {
-        let s = this.$refs.selection.data;
-        let p = this.onPageSelection;
-        s.map(item => {
-          p = p.filter(f => {
-            return f.userId !== item.userId;
-          });
-        });
-        this.onPageSelection = p;
-      } else {
-        let obj = {};
-        this.onPageSelection.push(...selection);
-        //数组去重
-        this.onPageSelection = this.onPageSelection.reduce((cur, next) => {
-          obj[next.userId] ? "" : (obj[next.userId] = true && cur.push(next));
-          return cur;
-        }, []);
-      }
-    },
-    //全选
-    onSelectAll(selection) {
-      let obj = {};
-      //触发全选事件
-      //全选
-      this.onPageSelection.push(...selection);
-      //数组去重
-      this.onPageSelection = this.onPageSelection.reduce((cur, next) => {
-        obj[next.userId] ? "" : (obj[next.userId] = true && cur.push(next));
-        return cur;
-      }, []);
-    },
-    //单选取消
-    onSelectCancel(selection, row) {
-      this.onPageSelection = this.onPageSelection.filter(f => {
-        return f.userId !== row.userId;
-      });
-    },
     //展示所有用户
     showUserModal() {
       this.showModal = true;
-      this.searchValue = '';
-      this.onPageSelection = []; //清空选中的用户
-      this.getAllUsersData();
-    },
-    //获取所有用户数据
-    getAllUsersData(filter) {
-      let relFilter = filter?filter:JSON.stringify([{operator:"ne",value:this.userId,property:"userId"}]);
-      this.userLoading = true;
-      getAllUsers(this.lowUser.allUserpageSize,this.lowUser.usercurrentPage,relFilter).then(res => {
-        this.userData = res.tableContent;
-        this.lowUser.usertotal = res.dataCount;
-        this.userLoading = false;
-
-        if (this.onPageSelection.length > 0) {
-            this.userData.map(item => {
-              this.onPageSelection.map(sel => {
-                if (item.userId === sel.userId) {
-                  item._checked = true;
-                }
-              });
-            });
-          }
-      })
-    },
-    //查询用户
-    userFilter() {
-      let filter = JSON.stringify([{operator_1:"like",value_1:this.searchValue,property_1:"nickname",link:"or",operator_2:"like",value_2:this.searchValue,property_2:"userCode"},
-      {operator:"ne",value:this.userId,property:"userId"}
-      ]);
-      this.getAllUsersData(filter);
     }
   },
   mounted() {

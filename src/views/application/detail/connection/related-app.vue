62a1dba2e32964f6fab94a1af0d3725451b23e04
wangxiaoying <cherry.wang@refordom.com>
2018-12-19 16:12:44 +0800

    修改应用相关应用样式

--- src/views/application/detail/connection/related-app.vue
+++ src/views/application/detail/connection/related-app.vue
@@ -11,14 +11,14 @@
     <Row class="related-app-content" :gutter="8">
       <draggable v-model="relatedApps" :options="dragOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false">
         <transition-group type="transition" :name="'flip-list'">
-        <Col :xs="24" :sm="12" :md="8" :lg="4" v-for="(app,index) of relatedApps" :key="index" style="margin-bottom:5px">
+        <Col :xs="24" :sm="12" :md="8" :lg="5" v-for="(app,index) of relatedApps" :key="index" style="margin-bottom:5px">
           <Card class="app-card">
             <img class="card-img" :src="app.icon" />
             <div class="card-right">
-              <Checkbox @on-change="changeAppStatus(app,index)" class="card-check" :disabled="!isAdmin" :value="app.STATUS===1?true:false"></Checkbox>
               <p class="card-name">{{ app.listName }}</p>
               <p class="card-type">{{ app.transName }}</p>
             </div>
+            <Checkbox @on-change="changeAppStatus(app,index)" class="card-check" :disabled="!isAdmin" :value="app.STATUS===1?true:false"></Checkbox>
           </Card>
         </Col>
         </transition-group>

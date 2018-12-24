537216158d3785bc061d66e879ca05c7295135c3
谭国正 <zheng.tan@refordom.com>
2018-12-24 16:01:26 +0800

    新增组织关系图

--- src/views/addressBook/organization/tree-chart/tree-chart.vue
+++ src/views/addressBook/organization/tree-chart/tree-chart.vue
@@ -1,62 +1,93 @@
-<style >
-    .tree-container{
-        background-color: #fff;
-        min-height: 500px
-    }
+<style lang="less">
+@import './tree-chart.less';
 </style>
 
 <template>
-    <div class="tree-container" id="chartContainerId"></div>
+    <div class="tree-container" id="chartContainerId" ></div>
 </template>
 
 <script>
-require('orgchart')
+import {getGroupData} from "@/services/addressBookService.js";
 export default {
     
     data(){
         return {
-
+          datasource:{
+            groupName: '组织架构',
+            groupId:7,
+            creator:" ",
+            children:[]
+          }
         };
     },
 
-    created(){
-       
+    methods:{
+      getGroupData(id){
+         const nodeTemplate = function(data) {
+          return `
+            <div class="org-item org-item_${data.groupId}" id=${data.groupId}>
+              <img class="org-item-img" src=${data.principalPic?data.principalPic:'resources/images/icon/user.png'} id=${data.groupId}></img>
+              <div class="org-item-content" id=${data.groupId}>
+                <a id=${data.groupId} href="/addressBook/organization/detail/${data.groupId}">${data.groupName}</a>
+                <div id=${data.groupId}>${data.creator?data.creator:" "}</div>
+              </div>
+            </div>
+          `;
+        };
+        getGroupData(id).then(res=>{
+          this.datasource.children = [...res.tableContent];
+          let oc = $('#chartContainerId').orgchart({
+            'data' : this.datasource,
+            'nodeTemplate': nodeTemplate
+          });
+          $('.org-item_7')[0].setAttribute('expand',true);
+
+          oc.$chartContainer.on('click', '.node', (e,f)=>{
+            let target = e.target || e.srcElement,
+                id = target.getAttribute('id'),
+                node = $('.org-item_'+id);
+            if(node[0].getAttribute('expand') !== 'true'){
+              this.handleExpandChildNode(node,oc,id);
+            }else{
+             this.handleDeleteChildNode(node,oc,id);
+            }
+          });
+        })
+      },
+
+      handleExpandChildNode(node,oc,id){
+         getGroupData(id).then(res=>{
+            if(res.dataCount>0){
+              oc.addChildren(node, res['tableContent'].map(function (item) {
+                return item;
+              }));
+              node[0].setAttribute('expand',true);
+            }else{
+              this.$Message.info('该组织下面没有下级组织!');
+            }
+         })
+      },
+
+      handleDeleteChildNode(node,oc,id){
+        getGroupData(id).then(res=>{
+            if(res.dataCount>0){
+              res['tableContent'].map(function (item) {
+                 oc.removeNodes($('.org-item_'+item.groupId));
+              })
+              node[0].setAttribute('expand',false);
+            }else{
+              this.$Message.info('该组织下面没有下级组织!');
+            }
+         })
+      }
     },
-    mounted(){
-            var datasource = {
-      'name': 'Lao Lao',
-      'title': 'general manager',
-      'office': '白城',
-      'children': [
-        { 'name': 'Bo Miao', 'title': 'department manager', 'office': '北京' },
-        { 'name': 'Su Miao', 'title': 'department manager', 'office': '北戴河',
-          'children': [
-            { 'name': 'Tie Hua', 'title': 'senior engineer', 'office': '北戴河' },
-            { 'name': 'Hei Hei', 'title': 'senior engineer', 'office': '北戴河' }
-          ]
-        },
-        { 'name': 'Yu Jie', 'title': 'department manager', 'office': '长春' },
-        { 'name': 'Yu Li', 'title': 'department manager', 'office': '长春' },
-        { 'name': 'Hong Miao', 'title': 'department manager', 'office': '长春' },
-        { 'name': 'Yu Wei', 'title': 'department manager', 'office': '长春' },
-        { 'name': 'Chun Miao', 'title': 'department manager', 'office': '长春' },
-        { 'name': 'Yu Tie', 'title': 'department manager', 'office': '长春' }
-      ]
-    };
 
-    var nodeTemplate = function(data) {
-      return `
-        <div><img src="resources/images/icon/user.png" width=30></div>
-        <span class="office">${data.office}</span>
-        <div class="title">${data.name}</div>
-        <div class="content">${data.title}</div>
-      `;
-    };
+   mounted(){
+    
+     this.getGroupData(7);
+    
 
-    var oc = $('#chartContainerId').orgchart({
-      'data' : datasource,
-      'nodeTemplate': nodeTemplate
-    });
+    
     }
 
    

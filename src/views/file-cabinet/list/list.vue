641e3c4cba89aa5d338c132977f67a43e2c0d00e
wangxiaoying <cherry.wang@refordom.com>
2019-01-08 11:18:52 +0800

    提交

---
+++ src/views/file-cabinet/list/list.vue
@@ -0,0 +1,30 @@
+<style lang="less" scoped>
+  @import "./list.less";
+</style>
+
+<template>
+    <div class="file">
+        <div class="toolbar">
+            
+        </div>
+        <div class="subarea"></div>
+    </div>
+</template>
+
+<script>
+import {  } from "@/services/workGuideService.js";
+
+export default {
+  name: "fileCabinetList",
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+  },
+  mounted() {
+  }
+};
+</script>
+
--- src/views/file-cabinet/router.js
+++ src/views/file-cabinet/router.js
@@ -4,48 +4,24 @@ export const fileCabinetRouter = {
     meta: {
         title: '文件柜'
     },
-    redirect: '/fileCabinet/subarea',
-    component: () => import('@/views/work-guide/layout.vue'),
+    redirect: '/fileCabinet/list',
+    component: () => import('@/views/file-cabinet/layout.vue'),
     children: [
         {
             path: 'list',
-            name: 'wokdGuideList',
+            name: 'fileCabinetList',
             meta: {
-                title: '作业指导列表',
+                title: '文件柜分区',
             },
-            component: () => import('@/views/work-guide/list/list.vue'),
+            component: () => import('@/views/file-cabinet/list/list.vue'),
         },
         {
             path: 'detail/:id',
-            name: 'wokdGuideDetail',
+            name: 'fileCabinetDetail',
             meta: {
-                title: '作业指导详情',
+                title: '文件柜分区详情',
             },
-            component: () => import('@/views/work-guide/detail/detail.vue'),
-        },
-        {
-            path: 'add',
-            name: 'wokdGuideAdd',
-            meta: {
-                title: '作业指导新增',
-            },
-            component: () => import('@/views/work-guide/detail/detail.vue'),
-        },
-        {
-            path: 'view/:id',
-            name: 'wokdGuideView',
-            meta: {
-                title: '作业指导查看',
-            },
-            component: () => import('@/views/work-guide/view/view.vue'),
-        },
-        {
-            path: 'step/:id',
-            name: 'wokdGuideStep',
-            meta: {
-                title: '作业指导步骤',
-            },
-            component: () => import('@/views/work-guide/step/step.vue'),
+            component: () => import('@/views/file-cabinet/detail/detail.vue'),
         }
     ]
 }

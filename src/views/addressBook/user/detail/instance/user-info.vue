af8a1471541a7c2e5f8f7a8844f454f9158ee440
snack <snack.huang@refordom.com>
2019-07-02 18:17:21 +0800

    fixbug:解决手机号不兼容166开头的手机号码

--- src/views/addressBook/user/detail/instance/user-info.vue
+++ src/views/addressBook/user/detail/instance/user-info.vue
@@ -65,7 +65,7 @@
                         style="width:60%">
                     </Input>
                 </FormItem>
-                <FormItem label="往来关系：">
+                <FormItem label="往来关系标签：">
                     <Select 
                         multiple 
                         style="width:60%" 
@@ -324,7 +324,7 @@ export default {
             required: true,
             message: "手机号输入不合法",
             trigger: "blur",
-            pattern: /^[1][3,4,5,7,8,9][0-9]{9}$/
+            pattern: /^[1][3,4,5,6,7,8,9][0-9]{9}$/
           }
         ],
         email: [

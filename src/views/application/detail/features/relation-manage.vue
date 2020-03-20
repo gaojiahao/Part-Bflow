019f247654bc1ebcb993f53436c07dfa64c674c5
wangxiaoying <cherry.wang@refordom.com>
2020-03-20 16:31:11 +0800

    优化往来和费用功能

--- src/views/application/detail/features/relation-manage.vue
+++ src/views/application/detail/features/relation-manage.vue
@@ -32,13 +32,25 @@
           <Table border stripe :columns="columns" :data="data">
             <template slot-scope="{ row }" slot="value">
                 <span v-if="!row.isEdit">{{ row.value }}</span>
-                <Select v-else v-model="row.value" transfer style="width:200px">
+                <Select 
+                  v-else 
+                  filterable 
+                  v-model="row.value"
+                  @on-change="onAppSelectChange" 
+                  transfer 
+                  style="width:200px">
                     <Option v-for="item in dataList" :value="item.value" :key="item.value">{{ item.label }}</Option>
                 </Select>
             </template>
             <template slot-scope="{ row }" slot="cashFlowName">
                 <span v-if="!row.isEdit">{{ row.cashFlowName }}</span>
-                <Select v-else v-model="row.cashFlowName" @on-change="onSelectChange(row,$event)" transfer style="width:200px">
+                <Select 
+                  v-else 
+                  v-model="row.cashFlowName" 
+                  filterable 
+                  @on-change="onSelectChange(row,$event)" 
+                  transfer 
+                  style="width:200px">
                     <Option v-for="item in cashFlowList" :value="item.value" :key="item.value">{{ item.label }}</Option>
                 </Select>
             </template>
@@ -98,6 +110,16 @@ export default {
             this.$set(row, "cashFlowId", item.cashFlowId);
             return;
           }
+        }
+      },
+      onAppSelectChange(value) {
+        let testArray = [];
+        this.dataList.forEach(item => {
+          testArray.push(item.label);
+        })
+        if(testArray.indexOf(value) > -1){
+          this.$Message.error(`${this.appName}：<b>${value}</b>已存在`);
+          return;
         }
       },
       editRelation(row) {
@@ -168,17 +190,16 @@ export default {
           }
       },
       createColumns() {
-        let appName = '费用子类',
-            columns = [];
-
+        let columns = [];
+        this.appName = '费用子类';
         if(this.listId === 'c0375170-d537-4f23-8ed0-a79cf75f5b04') {
-          appName = '关系标签';
+          this.appName = '关系标签';
           this.type = 'dealer';
         }
         columns = [
-          {title: appName,slot: 'value'},
+          {title: this.appName,slot: 'value'},
           {title: '现金流项目',slot: 'cashFlowName'},
-          {title: '现金流项目ID',key: 'cashFlowId'},
+          // {title: '现金流项目ID',key: 'cashFlowId'},
           {title: '排序',slot: 'sort'}
         ];
         this.isAdmin && columns.push({title: '动作',slot: 'action'});

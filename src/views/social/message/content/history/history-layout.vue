37b543bbabc95f28a544a9573ca01a9a5b397cdb
snack <snack.huang@refordom.com>
2020-04-10 16:47:09 +0800

    解决查看所有消息路由问题

--- src/views/social/message/content/history/history-layout.vue
+++ src/views/social/message/content/history/history-layout.vue
@@ -6,18 +6,18 @@
         
         <Row type="flex" justify="space-around" class="message-history-header" >
              <Col span="6">
-                <router-link :to="{ name:'all'}">
-                    <Button size=small  :type="$route.name=='all'?'primary':'text'" >全部</Button>
+                <router-link :to="{ name:'all',query:$route.query}">
+                    <Button size=small  :type="$route.name=='all'?'primary':'all'" >全部</Button>
                 </router-link>
             </Col>
             <Col span="6">
-                <router-link :to="{ name:'files'}">
-                    <Button size=small  :type="$route.name=='files'?'primary':'text'" >文件</Button>
+                <router-link :to="{ name:'files',query:$route.query}">
+                    <Button size=small  :type="$route.name=='files'?'primary':'files'" >文件</Button>
                 </router-link>
             </Col>
             <Col span="6">
-                <router-link :to="{ name:'images'}">
-                    <Button size=small :type="$route.name=='images'?'primary':'text'">图片/视频</Button>
+                <router-link :to="{ name:'images' ,query:$route.query}">
+                    <Button size=small :type="$route.name=='images'?'primary':'images'">图片/视频</Button>
                 </router-link>
             </Col>
         </Row>

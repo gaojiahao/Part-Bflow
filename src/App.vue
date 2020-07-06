e1798b9e853822c574223c8613db3d2f1de01e78
snack <snack.huang@refordom.com>
2020-07-06 16:54:43 +0800

    fixbug：解决集成到Ext时，实例详情日志任务没有完全显示的Bug

--- src/App.vue
+++ src/App.vue
@@ -52,12 +52,10 @@ html,body{
     width: 100%;
     height: 100%;
     background-color: #f0f0f0;
-    overflow: hidden;
 }
 .app-main{
     width: 100%;
     height: 100%;
-    overflow-y: auto;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;

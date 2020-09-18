379f50bc3828ed821d08837740ac563c304f2f3f
gaojiahao <zhoupeiguang@roletask.com>
2020-09-18 11:53:12 +0800

    fixbug(修复复制表格内容bug；复制图片显示只截取部分显示)

--- src/components/discussion/publish.vue
+++ src/components/discussion/publish.vue
@@ -606,7 +606,7 @@ export default {
             const that = this;
             this.$refs.editor.addEventListener("paste",  (e)=> {
                 let clipboardData = e.clipboardData;
-                let ua = window.navigator.userAgent
+                let ua = window.navigator.userAgent;
                 if ( !(clipboardData && clipboardData.items) ) {
                     return;
                 }
@@ -624,12 +624,15 @@ export default {
                 for (let i = 0, len = clipboardData.items.length; i < len; i++) {
                     let item = clipboardData.items[i];
                     if (item.kind === "file") {
+                        e.preventDefault(); //该方法将通知 Web 浏览器不要执行与事件关联的默认动作（如果存在这样的动作）。
+                                            //例如，如果 type 属性是 "submit"，在事件传播的任意阶段可以调用任意的事件句柄，通过调用该方法，可以阻止提交表单。
+                                            //注意，如果 Event 对象的 cancelable 属性是 fasle，那么就没有默认动作，或者不能阻止默认动作。无论哪种情况，调用该方法都没有作用。
                         let f= item.getAsFile();
                         let reader=new FileReader()
                         //读取完成
                         reader.onload= (e)=> {
                             that.uploadImageByBase64('ab',e.target.result);
-                        
+                            clipboardData.clearData('Text');
                         }
                         reader.readAsDataURL(f)
                     }
